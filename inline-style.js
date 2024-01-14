ab9d2d506239aba00164f66a1ac88488793542a7
navyagurrala <145953254+navyagurrala@users.noreply.github.com>
2024-01-14 11:44:40 +0530

    commit

---
+++ inline-style.js
@@ -0,0 +1,20 @@
+
+
+
+
+function Inlinestyle(){
+
+
+    //  const style={
+    //     background:"black",
+    //     color:"white"
+    //  }
+    return(
+        <>
+        
+        <h1 >navya b.tech</h1>
+        </>
+    )
+}
+
+export default Inlinestyle
---
+++ module-styles.js
@@ -0,0 +1,14 @@
+
+
+import  css from "./style.module.css"
+
+
+ const Modulestyles=()=>{
+    return(
+        <>
+        
+        <h1 className={css.name}>module styles</h1>
+        </>
+    )
+ }
+ export default Modulestyles
