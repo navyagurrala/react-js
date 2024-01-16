d8c3e2da629c9d014e32867064fb65e6146fd93d
navyagurrala <145953254+navyagurrala@users.noreply.github.com>
2024-01-16 17:55:49 +0530

    commit

---
+++ card-flex.js
@@ -0,0 +1,22 @@
+import Mapmethod from "./mapmethod"
+import Webpage from "./webpage-bootstrap"
+import Bootstrap from "./firstbootstrap"
+import Footer from "./footer"
+import Tablestyle from "./tablestyles"
+import Formstyling from "./formstyling"
+
+
+
+const Display=()=>{
+    return(
+        <>
+        <Webpage></Webpage>
+        <Bootstrap/>
+        <Footer/>
+        <Tablestyle/>
+        <Formstyling/>
+        </>
+    )
+}
+
+export default Display
